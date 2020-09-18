4c298399ba275b6786e596de6cf286e8c9062509
chenwu <chenwu@meitunmama.com>
2020-09-18 19:29:10 +0800

    提交kafka学习

---
+++ src/main/java/kafkaStudy/unit4/ProducerTest.java
@@ -0,0 +1,10 @@
+package kafkaStudy.unit4;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+
+public class ProducerTest {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/kafkaStudy/unit4/readme.txt
@@ -0,0 +1,1 @@
+本章介绍kafka producer端的配置
