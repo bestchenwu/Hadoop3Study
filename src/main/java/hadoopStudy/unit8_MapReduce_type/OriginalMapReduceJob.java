82e3cf0629d958f0843fdd2f8b215cde8473953d
bestchenwu <bestchenwu@163.com>
2020-10-05 09:34:28 +0800

    提交原始的MapReuduce Job

--- src/main/java/hadoopStudy/unit8_MapReduce_type/OriginalMapReduceJob.java
+++ src/main/java/hadoopStudy/unit8_MapReduce_type/OriginalMapReduceJob.java
@@ -2,6 +2,7 @@ package hadoopStudy.unit8_MapReduce_type;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
@@ -19,9 +20,15 @@ public class OriginalMapReduceJob extends Configured implements Tool {
     @Override
     public int run(String[] args) throws Exception {
         Configuration conf = new Configuration();
+        FileSystem fs = FileSystem.get(conf);
         Job job = Job.getInstance(conf, "OriginalMapReduceJob");
         FileInputFormat.setInputPaths(job, new Path(args[0]));
-        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+        Path outputPath = new Path(args[1]);
+        if(fs.exists(outputPath)){
+            fs.delete(outputPath,true);
+        }
+        FileOutputFormat.setOutputPath(job, outputPath);
+        //默认的mapper 输入key是行偏移量,value是值,输出也是原封不动的输出(key,value)
         boolean result = job.waitForCompletion(true);
         return result ? 0 : -1;
     }
