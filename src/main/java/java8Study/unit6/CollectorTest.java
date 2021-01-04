7633eb9cbf1e73b18de6473852b08cd6b951a80d
chenwu <chenwu@meitunmama.com>
2021-01-04 18:49:08 +0800

    提交收集器

--- src/main/java/java8Study/unit6/CollectorTest.java
+++ src/main/java/java8Study/unit6/CollectorTest.java
@@ -2,16 +2,15 @@ package java8Study.unit6;
 
 import java8Study.unit4.Dish;
 
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 import java.util.stream.Collectors;
+
 import static java.util.stream.Collectors.*;
+
 public class CollectorTest {
 
     public static void main(String[] args) {
-        List<Dish> list = Arrays.asList(new Dish("dish1", false, 18, Dish.Type.FISH), new Dish("dish2", true, 31, Dish.Type.MEAT),new Dish("dish3",true,9, Dish.Type.OTHER));
+        List<Dish> list = Arrays.asList(new Dish("dish1", false, 18, Dish.Type.FISH), new Dish("dish2", true, 31, Dish.Type.MEAT), new Dish("dish3", true, 9, Dish.Type.OTHER));
 //        Long count = list.stream().collect(Collectors.counting()); //等价于list.stream().count()
 //        System.out.println("count=" + count);
         //获取流中能量最高的元素
@@ -23,7 +22,16 @@ public class CollectorTest {
 //        }
 
         //获取能量总和
-        Integer totalCalories = list.stream().collect(summingInt(Dish::getCalories));
-        System.out.println(totalCalories);
+//        Integer totalCalories = list.stream().collect(summingInt(Dish::getCalories));
+//        System.out.println(totalCalories);
+        //获取能量最大的Dish
+//        Optional<Dish> optionalDish = list.stream().collect(reducing((d1, d2) -> d1.getCalories() > d2.getCalories() ? d1 : d2));
+//        if(optionalDish.isPresent()){
+//            System.out.println("max calory dish:"+optionalDish.get());
+//        }
+        //分组
+        Map<String, List<Dish>> collectMap
+                = list.stream().collect(groupingBy(Dish::getName));
+        System.out.println(collectMap);
     }
 }
