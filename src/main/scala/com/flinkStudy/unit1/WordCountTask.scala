5325df44427938dc968b69eaf444af8fbf55f708
bestchenwu <bestchenwu@163.com>
2021-06-19 20:16:53 +0800

    提交计数

--- src/main/scala/com/flinkStudy/unit1/WordCountTask.scala
+++ src/main/scala/com/flinkStudy/unit1/WordCountTask.scala
@@ -1,6 +1,10 @@
 package com.flinkStudy.unit1
 
+import java.io.File
+
+import common.constants.SymbolConstants
 import org.apache.flink.api.scala._
+import org.apache.flink.core.fs.FileSystem.WriteMode
 
 /**
  * 单词计数任务
@@ -10,8 +14,11 @@ import org.apache.flink.api.scala._
 object WordCountTask {
 
   def main(args: Array[String]): Unit = {
-    val env = ExecutionEnvironment.createRemoteEnvironment("master", 45491, "")
+    val env = ExecutionEnvironment.getExecutionEnvironment
+    //val env = ExecutionEnvironment.createRemoteEnvironment("master", 34909)
     val dataset = env.readTextFile("hdfs://master:9000/hadoop/temperature.txt", "UTF-8")
-    dataset.flatMap(_.split(",")).map((_,1))
+    val groupDataSet = dataset.flatMap(_.split(",")).map((_, 1)).groupBy(_._1).reduce((item1, item2) => (item1._1, item1._2 + item2._2))
+    groupDataSet.writeAsCsv("/data/logs/flink/wordCount.csv", SymbolConstants.SYMBOL_HH, SymbolConstants.SYMBOL_DH, WriteMode.OVERWRITE)
+    env.execute("WordCountTask")
   }
 }
