88cee0756d44f22d867f514c42c0e158bf5fe2b2
bestchenwu <bestchenwu@163.com>
2022-03-20 20:07:19 +0800

    提交过滤器

--- src/main/scala/com/flinkTheory/unit4/Every3ElementOutputTask.scala
+++ src/main/scala/com/flinkTheory/unit4/Every3ElementOutputTask.scala
@@ -1,11 +1,10 @@
 package com.flinkTheory.unit4
 
-import org.apache.commons.lang.StringUtils
 import org.apache.flink.api.common.serialization.SimpleStringSchema
 import org.apache.flink.runtime.state.filesystem.FsStateBackend
 import org.apache.flink.streaming.api.TimeCharacteristic
 import org.apache.flink.streaming.api.scala._
-import org.apache.flink.streaming.api.windowing.assigners.{TumblingEventTimeWindows, TumblingProcessingTimeWindows}
+import org.apache.flink.streaming.api.windowing.assigners.{ TumblingProcessingTimeWindows}
 import org.apache.flink.streaming.api.windowing.time.Time
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer
 import org.apache.kafka.clients.consumer.ConsumerConfig
@@ -31,7 +30,7 @@ object Every3ElementOutputTask {
     properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer")
     properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer")
     val topic1 = "test"
-    val userBehaviorStream = env.addSource(new FlinkKafkaConsumer[String](topic1, new SimpleStringSchema(), properties)).filter(StringUtils.isNotBlank(_)).filter(_.split(",").size == 2).map(item => {
+    val userBehaviorStream = env.addSource(new FlinkKafkaConsumer[String](topic1, new SimpleStringSchema(), properties)).filter(_.nonEmpty).filter(_.split(",").size == 2).map(item => {
       val array = item.split(",")
       (array(0).toLong, array(1))
     })
